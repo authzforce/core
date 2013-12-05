29b9edea9f2f74e3cade05ea85491aa394fc1d03
Romain Ferrari <romain.ferrari@thalesgroup.com>
2013-12-05 09:14:26 +0100

    Audit logs added. Marshal to json so it can be parsed easily by logstash.

--- src/main/java/com/thalesgroup/authzforce/audit/AuditLogs.java
+++ src/main/java/com/thalesgroup/authzforce/audit/AuditLogs.java
@@ -15,113 +15,123 @@
  */
 package com.thalesgroup.authzforce.audit;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.AnnotationIntrospector;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.xacml.Rule;
-import com.sun.xacml.xacmlv3.Policy;
+import com.thalesgroup.authzforce.audit.schema.pdp.AuditedPolicy;
+import com.thalesgroup.authzforce.audit.schema.pdp.AuditedRule;
 
 /**
  * Audit logging class
  * 
  * Note: to disable audit logging, just change log level for logger named
- * 'com.thalesgroup.authzforce.audit.AuditLogs' in the SLF4J logger configuration file. Use level <=
- * INFO to enable, level > INFO to disable. Other options on this logger may be configured in the
- * logging configuration file to customize audit logging.
+ * 'com.thalesgroup.authzforce.audit.AuditLogs' in the SLF4J logger
+ * configuration file. Use level <= INFO to enable, level > INFO to disable.
+ * Other options on this logger may be configured in the logging configuration
+ * file to customize audit logging.
  * 
  */
-public final class AuditLogs
-{
+public final class AuditLogs {
 
 	/**
 	 * Logger used for all classes
 	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(AuditLogs.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(AuditLogs.class);
+
+	private static final String HASH_ALG = "MD5";
 
 	protected static volatile AuditLogs INSTANCE;
-	protected static HashMap<String, AuditLog> audits;
 
-	private AuditLogs()
-	{
-		audits = new HashMap<String, AuditLog>();
+	protected static HashMap<String, com.thalesgroup.authzforce.audit.schema.pdp.AuditLog> audits;
+
+	private AuditLogs() {
+		audits = new HashMap<String, com.thalesgroup.authzforce.audit.schema.pdp.AuditLog>();
 	}
 
-	public synchronized static AuditLogs getInstance()
-	{
-		if (INSTANCE == null)
-		{
+	public synchronized static AuditLogs getInstance() {
+		if (INSTANCE == null) {
 			INSTANCE = new AuditLogs();
 		}
 		return INSTANCE;
 	}
 
-	public synchronized static AuditLogs remove()
-	{
-		if (INSTANCE != null)
-		{
+	public synchronized static AuditLogs remove() {
+		if (INSTANCE != null) {
 			INSTANCE = new AuditLogs();
 		}
 		return INSTANCE;
 	}
 
-	public synchronized static Map<String, AuditLog> getAudits()
-	{
+	public synchronized static Map<String, com.thalesgroup.authzforce.audit.schema.pdp.AuditLog> getAudits() {
 		return audits;
 	}
+	
+	public synchronized static void clearAudits() {
+		audits.clear();
+	}
 
-	public synchronized static void addAudit(AuditLog audit)
-	{
-		// BASE64Encoder enc = new sun.misc.BASE64Encoder();
-		// String toEncode = audit.getRequest().toString() + audit.getDate();
-		// String base64 = enc.encode(toEncode.getBytes());
-		if (audits == null)
-		{
-			audits = new HashMap<String, AuditLog>();
+	public synchronized static void addAudit(
+			com.thalesgroup.authzforce.audit.schema.pdp.AuditLog audit) {
+		if (audits == null) {
+			audits = new HashMap<String, com.thalesgroup.authzforce.audit.schema.pdp.AuditLog>();
 		}
-		audit.setId(String.valueOf(audit.getRequest().hashCode()));
-		if (audits.containsKey(audit.getId()))
-		{
+		try {
+			MessageDigest digest = MessageDigest.getInstance(HASH_ALG);
+			byte[] hash = digest
+					.digest(String.valueOf(
+							audit.getDate() + audit.getRequest().hashCode())
+							.getBytes());
+			String id = byte2String(hash);
+			audit.setId(id);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		if (audits.containsKey(audit.getId())) {
 			audits.put(audit.getId(), updateAudit(audit));
-		} else
-		{
+		} else {
 			audits.put(audit.getId(), audit);
 		}
 	}
 
-	private static AuditLog updateAudit(AuditLog newAudit)
-	{
-		AuditLog updatedAudit = audits.get(newAudit.getId());
+	private static com.thalesgroup.authzforce.audit.schema.pdp.AuditLog updateAudit(
+			com.thalesgroup.authzforce.audit.schema.pdp.AuditLog newAudit) {
+		com.thalesgroup.authzforce.audit.schema.pdp.AuditLog updatedAudit = audits
+				.get(newAudit.getId());
 		// Updating rules and decision for rules
-		if (newAudit.getRules().size() > 0)
-		{
+		if (newAudit.getRules().size() > 0) {
 			int i = 0;
-			for (Rule ruleElt : newAudit.getRules())
-			{
+			for (AuditedRule ruleElt : newAudit.getRules()) {
 				// We don't want any doublon. TODO: Maybe use a set ?
-				if (!updatedAudit.getRules().contains(ruleElt))
-				{
-					updatedAudit.addRule(ruleElt);
+				if (!updatedAudit.getRules().contains(ruleElt)) {
+					updatedAudit.getRules().add(ruleElt);
 					// If we've got a rule, we've got a decision
-					updatedAudit.addResultRule(newAudit.getResultRule().get(i));
+//					 updatedAudit.getResultRules().add(newAudit.getResultRules().get(i));
 				}
 				i++;
 			}
 		}
 
-		if (newAudit.getMatchPolicies().size() > 0)
-		{
+		if (newAudit.getMatchedPolicies().size() > 0) {
 			int i = 0;
-			for (Policy policyElt : newAudit.getMatchPolicies())
-			{
+			for (AuditedPolicy policyElt : newAudit.getMatchedPolicies()) {
 				// We don't want any doublon. TODO: Maybe use a set ?
-				if (!updatedAudit.getMatchPolicies().contains(policyElt))
-				{
-					updatedAudit.addMatchPolicies(policyElt);
+				if (!updatedAudit.getMatchedPolicies().contains(policyElt)) {
+					updatedAudit.getMatchedPolicies().add(policyElt);
 					// If we've got a rule, we've got a decision
-					updatedAudit.addResultMatchPolicy(newAudit.getResultMatchPolicy().get(i));
+					// updatedAudit.getResultPolicies().add(newAudit.getResultPolicies().get(i));
 				}
 				i++;
 			}
@@ -131,32 +141,49 @@ public final class AuditLogs
 	}
 
 	/**
-	 * One part of the method needs to be synchronized as it modify the map containing the audits
-	 * logs. No one should update this map while it's being displayed and cleared.
+	 * One part of the method needs to be synchronized as it modify the map
+	 * containing the audits logs. No one should update this map while it's
+	 * being displayed and cleared.
 	 */
-	public void print()
-	{
-		Map<String, AuditLog> auditTmp = null;
-		synchronized (audits)
-		{
-			auditTmp = new HashMap<String, AuditLog>(audits);
-			audits.clear();
-		}
-		for (AuditLog auditElt : auditTmp.values())
-		{
-			StringBuilder sb = new StringBuilder();
-			sb.append(auditElt.print());
+	@Override
+	public String toString() {
+		StringWriter sw = new StringWriter();
+		try {
+			for (String hash : getAudits().keySet()) {
+				// JAXBContext.newInstance(AuditLog.class).createMarshaller().marshal(getAudits().get(hash),
+				// sw);
 
-			/**
-			 * To disable audit logging at this point, just change log level for logger named
-			 * 'com.thalesgroup.authzforce.audit.AuditLogs' in the SLF4J logger configuration Use
-			 * level <= INFO to enable, level > INFO to disable.
-			 */
-			LOGGER.info(sb.toString());
+				ObjectMapper mapper = new ObjectMapper();
+				AnnotationIntrospector introspector = new JacksonAnnotationIntrospector();
+				// make serializer use JAXB annotations (only)
+				mapper.getSerializationConfig().withAnnotationIntrospector(
+						introspector);
+				mapper.writeValue(sw, getAudits().get(hash));
+			}
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		// Releasing memory 
-		auditTmp = null;		
-//		System.out.println(sb);		
+		clearAudits();
+		return sw.toString();
+	}
+
+	private static String byte2String(byte[] hash) {
+		StringBuffer id = new StringBuffer();
+		for (int i = 0; i < hash.length; i++) {
+			if ((0xff & hash[i]) < 0x10) {
+				id.append("0" + Integer.toHexString((0xFF & hash[i])));
+			} else {
+				id.append(Integer.toHexString(0xFF & hash[i]));
+			}
+		}
+
+		return id.toString();
 	}
 }
