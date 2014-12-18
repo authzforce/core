4612814744ecce020555af2081e0aa9dd964345d
Romain Ferrari <romain.ferrari@thalesgroup.com>
2014-12-18 14:46:51 +0100

    Adding the two last test class in the testsuite. They are commented for now. It will be corrected after the release

--- src/test/java/com/thalesgroup/authzforce/pdp/core/test/impl/MainTest.java
+++ src/test/java/com/thalesgroup/authzforce/pdp/core/test/impl/MainTest.java
@@ -23,6 +23,7 @@ import com.sun.xacml.cond.SpecialMatchFunctionsTest;
 import com.sun.xacml.cond.StringConversionFunctionsTest;
 import com.sun.xacml.cond.StringFunctionsTest;
 import com.sun.xacml.xacmlv3.TestMatchAlg;
+import com.sun.xacml.xacmlv3.function.TestDateMathFunction;
 import com.sun.xacml.xacmlv3.function.TestMatchFunction;
 import com.sun.xacml.xacmlv3.function.TestStringFunction;
 
@@ -44,9 +45,9 @@ import com.sun.xacml.xacmlv3.function.TestStringFunction;
 		HigherOrderFunctionsTest.class, RegExpBasedFunctionsTest.class,
 		SpecialMatchFunctionsTest.class, TestMatchAlg.class,
 		TestMatchFunction.class, TestStringFunction.class,
-		/* TestDateMathFunction.class, */ConformanceV3.class, BasicV3_1.class,
+		/*TestDateMathFunction.class,*/ ConformanceV3.class, BasicV3_1.class,
 		BasicV3_2.class, BasicV3_3.class, BasicV3_4.class, BasicV3_5.class,
-		BasicFunctionV3.class })
+		BasicFunctionV3.class/*, BasicMultipleRequestV3.class */ })
 public class MainTest {
 	/**
 	 * the logger we'll use for all messages
