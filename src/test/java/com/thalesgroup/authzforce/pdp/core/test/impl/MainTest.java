20ef2a3938ce5020817631fa82604fe7fd5d01cd
Romain Ferrari <romain.ferrari@thalesgroup.com>
2015-01-29 13:55:29 +0100

    Unitary test regarding issue #27 Apply object marshalling

--- src/test/java/com/thalesgroup/authzforce/pdp/core/test/impl/MainTest.java
+++ src/test/java/com/thalesgroup/authzforce/pdp/core/test/impl/MainTest.java
@@ -40,6 +40,7 @@ import com.sun.xacml.cond.SetFunctionsTest;
 import com.sun.xacml.cond.SpecialMatchFunctionsTest;
 import com.sun.xacml.cond.StringConversionFunctionsTest;
 import com.sun.xacml.cond.StringFunctionsTest;
+import com.sun.xacml.xacmlv3.TestApply;
 import com.sun.xacml.xacmlv3.TestMatchAlg;
 import com.sun.xacml.xacmlv3.function.TestDateMathFunction;
 import com.sun.xacml.xacmlv3.function.TestMatchFunction;
@@ -64,7 +65,7 @@ import com.sun.xacml.xacmlv3.function.TestStringFunction;
 		TestMatchFunction.class, TestStringFunction.class,
 		/* TestDateMathFunction.class, */ ConformanceV3.class, BasicV3_1.class,
 		BasicV3_2.class, BasicV3_3.class, BasicV3_4.class, BasicV3_5.class,
-		BasicFunctionV3.class/*, BasicMultipleRequestV3.class */ })
+		BasicFunctionV3.class/*, BasicMultipleRequestV3.class */, TestApply.class })
 public class MainTest {
 	/**
 	 * the logger we'll use for all messages
